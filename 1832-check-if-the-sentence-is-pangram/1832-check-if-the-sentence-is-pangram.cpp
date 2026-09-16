7042660fb87a904ce925ccd1c1c65d044f5b1ce4
Rajvansh Singh Atal <rajvanshatal@gmail.com>
2026-09-16 16:10:27 +0530

    Time: 0 ms (100%), Space: 8.8 MB (83.87%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        int mask = 0;
+
+        for (char c : sentence) {
+            mask |= (1 << (c - 'a'));
+        }
+
+        return mask == (1 << 26) - 1;
+    }
+};
